00b40cae40c12bd01488562bb58b3786225fb434
attila-tamas <attila.tamas.code@gmail.com>
2023-11-01 13:09:53 +0000

    chore: disable nuxt telemetry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  telemetry: false,
   devtools: { enabled: true },
   typescript: { shim: false },
   css: ["~/assets/css/main.scss"],
