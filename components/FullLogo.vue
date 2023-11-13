3b9da97d2af198331bddd9ea5419ddaffcc42c5b
attila-tamas <attila.tamas.code@gmail.com>
2023-11-13 20:18:55 +0000

    feat: add logo component

---
+++ components/FullLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtImg
+    src="/images/logo.webp"
+    width="168"
+    height="48"
+    sizes="112px xs:168px"
+    :placeholder="[168, 48, 50, 1]"
+    class="select-none"
+  />
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
