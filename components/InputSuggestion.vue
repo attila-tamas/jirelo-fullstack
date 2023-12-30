f75845253183113650fee9d267548923374ab52a
attila-tamas <attila.tamas.code@gmail.com>
2023-12-30 14:59:43 +0000

    feat: add InputSuggestion comp

---
+++ components/InputSuggestion.vue
@@ -0,0 +1,30 @@
+<template>
+  <AutoAnimate>
+    <v-alert
+      v-if="props.suggestion"
+      color="neutral-400"
+    >
+      <section class="break-all">
+        <span>Did you mean </span>
+        <span
+          class="underline hover:cursor-pointer hover:text-neutral-950"
+          @click="emit('suggestionAccepted')"
+        >
+          {{ props.suggestion }}
+        </span>
+        <span>?</span>
+      </section>
+    </v-alert>
+  </AutoAnimate>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps({
+    suggestion: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const emit = defineEmits(["suggestionAccepted"]);
+</script>
