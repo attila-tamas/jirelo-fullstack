4d61ba0849d5e2846438017f2c7ab627d8d23749
attila-tamas <attila.tamas.code@gmail.com>
2024-02-08 19:26:34 +0000

    feat: add NavLink comp

---
+++ components/NavLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink
+    v-bind="$attrs"
+    class="font-semibold hover:border-b-2 hover:border-primary-500"
+  >
+    {{ props.label }}
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps({
+    label: {
+      type: String,
+      required: true,
+    },
+  });
+</script>
