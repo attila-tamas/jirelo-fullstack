302c42e049bbcb07f2f9af8a6d062bbba5cf9c86
attila-tamas <attila.tamas.code@gmail.com>
2024-02-08 19:27:02 +0000

    feat: add SimpleSheet comp

---
+++ components/SimpleSheet.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-sheet
+    v-bind="$attrs"
+    class="!bg-neutral-50 p-8 !text-black dark:!bg-neutral-800 dark:!text-white"
+    :class="props.class"
+  >
+    <slot />
+  </v-sheet>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps({
+    class: {
+      type: [Object, String],
+      default: "",
+    },
+  });
+</script>
