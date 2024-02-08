a1437c2ca0c1563dcfb9f98bd9973fa2c0d1312c
attila-tamas <attila.tamas.code@gmail.com>
2024-02-08 19:27:15 +0000

    feat: add PageContainer comp

---
+++ components/PageContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container
+    v-bind="$attrs"
+    class="h-full !bg-neutral-100 dark:!bg-neutral-900"
+    :class="props.class"
+  >
+    <slot />
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps({
+    class: {
+      type: [Object, String],
+      default: "",
+    },
+  });
+</script>
