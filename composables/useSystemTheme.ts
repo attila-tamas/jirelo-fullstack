1d18595cf7f541ade461a4b1e078bb49213af0e9
attila-tamas <attila.tamas.code@gmail.com>
2024-02-08 19:23:18 +0000

    ref: simplify useSystemTheme

--- composables/useSystemTheme.ts
+++ composables/useSystemTheme.ts
@@ -3,31 +3,16 @@ export default () => {
   const theme = useTheme();
   const colorMode = useColorMode();
 
-  const colorScheme = ref("");
-
-  watch(colorScheme, async () => {
-    theme.global.name.value = getTheme();
-    colorMode.preference = systemStore.theme = colorScheme.value;
+  watch(colorMode, () => {
+    theme.global.name.value = systemStore.theme = colorMode.value;
   });
-
-  function getTheme(): string {
-    if (colorScheme.value === codeTable.theme.SYSTEM) {
-      return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? codeTable.theme.DARK
-        : codeTable.theme.LIGHT;
-    }
-    return colorScheme.value;
-  }
 
   return {
     getCurrent: (): string => {
-      return systemStore.theme;
-    },
-    getIsDark: (): boolean => {
-      return theme.global.current.value.dark;
+      return colorMode.value;
     },
     set: (newValue: string): void => {
-      colorScheme.value = newValue;
+      colorMode.preference = newValue;
     },
   };
 };
