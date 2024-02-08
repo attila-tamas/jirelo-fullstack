fcc091d95659837008ced4ec3f00d99f01334d3c
attila-tamas <attila.tamas.code@gmail.com>
2024-02-08 19:32:12 +0000

    ref: improve codeTable maintainability

--- utils/codeTable.ts
+++ utils/codeTable.ts
@@ -3,6 +3,11 @@ export default {
     FLAT: 0,
     DEFAULT: 2,
   },
+  iconSize: {
+    XS: 16,
+    SM: 20,
+    BASE: 24,
+  },
   registrationForm: {
     STEP_ONE: "step_one",
     STEP_TWO: "step_two",
@@ -11,10 +16,5 @@ export default {
     DARK: "dark",
     LIGHT: "light",
     SYSTEM: "system",
-  },
-  iconSize: {
-    XS: 16,
-    SM: 20,
-    BASE: 24,
   },
 };
